4cacb8a09cec46bc28108ee53d1fd45b95e9eb67
Bruno Camilo da Silva <b.camilosilva16@gmail.com>
2022-11-24 20:50:43 -0300

    Subida dos captulos 2.2 a 2.4

---
+++ 2.3-Modificando-nosso-primeiro-programa-Java/Welcome2.java
@@ -0,0 +1,10 @@
+// Figura 2.3: Welcome2.java
+// Imprimindo uma linha de texto com multiplas instruções.
+public class Welcome2{
+// método main inicia a execução do aplicativo Java
+	public static void main(String[] args){
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+		System.out.println("Welcome\nto\nJava\nProgramming!");
+  }// fim do método main
+}// fim da classe Welcome1
